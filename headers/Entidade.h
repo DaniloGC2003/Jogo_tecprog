03392ebeeb1d9aac1a9c9ee0bf71b8d079f8d70b
1moledu <105132366+1moledu@users.noreply.github.com>
2022-05-15 18:59:52 -0300

    Update Entidade.h

--- headers/Entidade.h
+++ headers/Entidade.h
@@ -1,16 +1,32 @@
 #pragma once
-#include <SFML/Graphics.hpp>
-#include "Gerenciador_grafico.h"
-class Entidade
-{
-private:
-	sf::RectangleShape hitbox;
-	Gerenciador_grafico* graficos;
-public:
-	Entidade();
-	Entidade(sf::Vector2f tamanho);
-	~Entidade();
-	void setTamanho(sf::Vector2f tam);
-	sf::RectangleShape* getHitbox();
-};
 
+#include "Ente.h"
+
+namespace Entities {
+
+    class Entidade : public Ente {
+    protected:
+        sf::Vector2f velocidade;
+    	sf::Vector2f hitbox;
+
+    public:
+	Entidade(ID id, sf::Vector2f pos, sf::Vector2f hit);
+    	Entidade();
+    	virtual ~Entidade();
+	    
+        void mudarPosicao(const sf::Vector2f pos);
+    	sf::Vector2f getPosicao() const;
+
+    	void setVelocidade(const sf::Vector2f vel);
+    	sf::Vector2f getVelocidade() const;
+
+    	void setHitbox(const sf::Vector2f hit);
+    	sf::Vector2f getHitbox() const;
+
+    	virtual void update(float dt) = 0;
+    	virtual void initialize() = 0;
+
+    	ID getId() const;
+    	void Move(sf::Vector2f vec);
+
+    };
